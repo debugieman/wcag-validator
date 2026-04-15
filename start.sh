b57b52b5e7316217b314d133ce1aa3e7fcee3a9c
debugieman <107075050+debugieman@users.noreply.github.com>
2026-04-15 08:26:02 +0200

    fix backend launch profile and angular analytics config

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting backend..."
-dotnet run --project src/Api/WcagAnalyzer.Api.csproj &
+dotnet run --project src/Api/WcagAnalyzer.Api.csproj --launch-profile https &
 
 echo "Starting frontend..."
 cd src/frontend && npm start
